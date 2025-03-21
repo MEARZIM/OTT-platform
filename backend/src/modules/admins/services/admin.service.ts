5c946c1a768c2dcf95a1ce6dd28f1dfff5dc2f7c
MEARZIM <asahaayan@gmail.com>
2025-03-21 16:06:50 +0530

    feat: integrate AWS S3 for file storage in admin services

--- backend/src/modules/admins/services/admin.service.ts
+++ backend/src/modules/admins/services/admin.service.ts
@@ -1,5 +1,7 @@
 import path from "path";
 import video from "../../../libs/mux";
+import s3 from "../../../libs/s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 
 class AdminServices {
@@ -7,6 +9,13 @@ class AdminServices {
 
         const filePath = path.join(__dirname,"../../../../", file.path);
        
+        const command = new PutObjectCommand({
+            Bucket: process.env.AWS_BUCKET_NAME!,
+            Key: "ayan.txt",
+            Body: "Awsome",
+        });
+
+        await s3.send(command);
 
         return filePath;
     }
