fcb1517fe6deb4ae17c2fcc6bc0c49237954486b
MEARZIM <asahaayan@gmail.com>
2025-03-23 18:56:45 +0530

    test: integrate Mux video asset creation with S3 upload in admin services

--- backend/src/modules/admins/services/admin.service.ts
+++ backend/src/modules/admins/services/admin.service.ts
@@ -1,3 +1,4 @@
+import video from "../../../libs/mux";
 import s3 from "../../../libs/s3";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 
@@ -13,7 +14,18 @@ class AdminServices {
 
         await s3.send(command);
 
-        return `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/videos/${fileName}`;
+        const s3url = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
+
+        const asset = await video.assets.create({
+            input: [{ url: s3url }],
+            playback_policy: ["public"],
+            test: false,
+            video_quality: "plus"
+        })
+        console.log(asset);
+
+        return "video url"
+        // return `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/videos/${fileName}`;
     }
 }
 
