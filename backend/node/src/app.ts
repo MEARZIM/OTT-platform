6120aa8f6d75a23b59e9a230b51b583e3d2994b6
MEARZIM <asahaayan@gmail.com>
2025-05-03 00:17:56 +0530

    fix: update CORS methods to include PATCH for enhanced API support

--- backend/node/src/app.ts
+++ backend/node/src/app.ts
@@ -19,7 +19,7 @@ const app = express();
 app.use(
     cors({
         origin: process.env.FRONTEND_URL, 
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "PATCH" ,"DELETE"],
         credentials: true,
     })
 );
