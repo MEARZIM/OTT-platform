bcb771c1b26a207447b86c593b0f22ae66f7604d
sayaniB <sayanibasu2232@gmail.com>
2025-05-06 22:52:20 +0530

    fix: added routing to user dashboard

--- frontend/src/pages/User/Dashboard/components/Banner.tsx
+++ frontend/src/pages/User/Dashboard/components/Banner.tsx
@@ -3,57 +3,51 @@ import {
     Carousel,
     CarouselContent,
     CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "../../../../components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay"
+import Autoplay from "embla-carousel-autoplay";
+import videoData from "../../Watch/data/VideoList";
+
+import { Link } from "react-router-dom";
 
 const Banner = () => {
-    const bannerContent = [
-        { title: "Undercover High School", image: "/assets/undercover-high-school.jpg" },
-        { title: "When Life Gives You Tangerines", image: "/assets/when-life-gives-you-tangerines.jpg" },
-        { title: "First Frost", image: "/assets/first-frost.jpg" },
-        { title: "The Potato Lab", image: "/assets/the-potato-lab.jpg" },
-        { title: "When The Stars Gossip", image: "/assets/when-the-stars-gossip.jpg" },
-        { title: "My Dearest Nemesis", image: "/assets/my-dearest-nemesis.jpg" },
-        { title: "Melo Movie", image: "/assets/melo-movie.jpg" },
-    ];
+  const bannerContent = videoData.slice(8, 15);
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
 
-    const plugin = useRef(
-        Autoplay({ delay: 2000, stopOnInteraction: true })
-    )
+  return (
+    <div className="relative h-[40vh] md:h-[50vh] mb-7 overflow-hidden">
+  <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent z-10 pointer-events-none"></div>
 
-    return (
-        <div className="relative h-[40vh] md:h-[50vh] mb-7 overflow-hidden">
-            <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent z-10"></div>
-            <Carousel
-                plugins={[plugin.current]}
-                className="w-full h-full"
-                onMouseEnter={plugin.current.stop}
-                onMouseLeave={plugin.current.reset}
-            >
-                <CarouselContent>
-                    {bannerContent.map(({ title, image }, index) => (
-                        <CarouselItem
-                            key={index}
-                            className="w-full h-[40vh] md:h-[50vh] transition-opacity duration-500"
-                        >
-                            <img
-                                src={image}
-                                alt={title}
-                                className="w-full h-full object-cover rounded-md"
-                            />
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
+  <Carousel
+    plugins={[plugin.current]}
+    className="w-full h-full"
+    onMouseEnter={plugin.current.stop}
+    onMouseLeave={plugin.current.reset}
+  >
+    <CarouselContent>
+      {bannerContent.map((video) => (
+        <CarouselItem
+          key={video.id}
+          className="w-full h-[40vh] md:h-[50vh] transition-opacity duration-500"
+        >
+          <Link to={`/player/${video.id}`} className="w-full h-full block">
+            <img
+              src={video.thumbnail}
+              alt={video.title}
+              className="w-full h-full object-cover rounded-md cursor-pointer"
+            />
+          </Link>
+        </CarouselItem>
+      ))}
+    </CarouselContent>
 
-                {/* <CarouselPrevious
-                    className="absolute left-2 top-1/2 -translate-y-1/2 size-10 font-extrabold bg-blue-200/50 border-none p-2 rounded-full text-blue-700 hover:bg-gray-800 cursor-pointer"
-                />
-                <CarouselNext
-                    className="absolute right-2 top-1/2 -translate-y-1/2 size-10 font-extrabold bg-blue-200/50 border-none p-2 rounded-full text-blue-700 hover:bg-gray-800 cursor-pointer"
-                /> */}
-            </Carousel>
-        </div>
-    );
+    <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 size-10 bg-blue-200/50 border-none p-2 rounded-full text-blue-700 hover:bg-gray-800 cursor-pointer" />
+    <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 size-10 bg-blue-200/50 border-none p-2 rounded-full text-blue-700 hover:bg-gray-800 cursor-pointer" />
+  </Carousel>
+</div>
+
+  );
 };
 
 export default Banner;
